cfedb6e8c4c9f6ecaf5e01691d389c47f6352683
Sasha Konovalova <sasha@managedbyq.com>
2018-09-25 11:06:22 -0400

    debounce decorator

--- mbq/atomiq/utils.py
+++ mbq/atomiq/utils.py
@@ -1,3 +1,36 @@
+import time
+
+from functools import wraps
+
+
 def time_difference_ms(start_datetime, end_datetime):
     diff_in_seconds = (end_datetime - start_datetime).total_seconds()
     return round(diff_in_seconds * 1000)
+
+
+def debounce(days=None, hours=None, minutes=None, seconds=None):
+    def wrapper(func):
+        last_run = 0
+        seconds_between_runs = 0
+
+        if seconds:
+            seconds_between_runs += seconds
+        if minutes:
+            seconds_between_runs += minutes * 60
+        if hours:
+            seconds_between_runs += hours * 60 * 60
+        if days:
+            seconds_between_runs += days * 24 * 60 * 60
+
+        @wraps(func)
+        def wrapped_func(*args, **kwargs):
+            now = time.time()
+
+            if last_run + seconds_between_runs < now:
+                last_run = now
+                return func(*args, **kwargs)
+
+            return
+
+        return wrapped_func
+    return wrapper
