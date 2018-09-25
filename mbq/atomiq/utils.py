b4bda2f99dcabca73725330b043dc13677da7108
Sasha Konovalova <sasha@managedbyq.com>
2018-09-25 15:25:48 -0400

    update tests and fix debouncer

--- mbq/atomiq/utils.py
+++ mbq/atomiq/utils.py
@@ -10,25 +10,21 @@ def time_difference_ms(start_datetime, end_datetime):
 
 def debounce(seconds=None, minutes=None, hours=None):
     def wrapper(func):
-        last_run = 0
-        seconds_between_runs = 0
+        func.seconds_between_runs = 0
+        func.last_run = 0
 
         if seconds:
-            seconds_between_runs += seconds
+            func.seconds_between_runs += seconds
         if minutes:
-            seconds_between_runs += minutes * 60
+            func.seconds_between_runs += minutes * 60
         if hours:
-            seconds_between_runs += hours * 60 * 60
+            func.seconds_between_runs += hours * 60 * 60
 
         @wraps(func)
         def wrapped_func(*args, **kwargs):
-            now = time.time()
-
-            if last_run + seconds_between_runs < now:
-                last_run = now
-                return func(*args, **kwargs)
-
-            return
+            if func.last_run + func.seconds_between_runs < time.time():
+                func(*args, **kwargs)
+                func.last_run = time.time()
 
         return wrapped_func
     return wrapper
